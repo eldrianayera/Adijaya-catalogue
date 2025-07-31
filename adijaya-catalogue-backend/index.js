ab9998b66bea86acd27db09b825759b67797a0c0
Eldrian <eldrianayera710@gmail.com>
2025-07-31 22:55:26 +0800

    feat(backend): create admin.js for admin CUD access

--- adijaya-catalogue-backend/index.js
+++ adijaya-catalogue-backend/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const productsRouter = require("./routes/products");
 const authRouter = require("./routes/auth"); //
+const adminRouter = require("./routes/admin");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +16,7 @@ app.use(express.json()); // Parse JSON request bodies
 // API routes
 app.use("/api/products", productsRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/admin", adminRouter);
 
 // Start server
 app.listen(PORT, () => {
