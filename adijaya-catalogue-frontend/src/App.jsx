baaa383b30e8cd48b03f1d0d833889c4e7ae789e
Eldrian <eldrianayera710@gmail.com>
2025-07-31 23:21:18 +0800

    fix(frontend): fix App.jsx

--- adijaya-catalogue-frontend/src/App.jsx
+++ adijaya-catalogue-frontend/src/App.jsx
@@ -1,32 +1,8 @@
-import React, { useState } from "react";
-import AdminLogin from "./AdminLogin";
+import AdminProducts from "./pages/AdminProducts";
+import VisitorProducts from "./pages/VisitorProducts";
 
 export default function App() {
-  const [role, setRole] = useState(localStorage.getItem("role") || null);
+  const isAdmin = false;
 
-  if (!role) {
-    // If not logged in, show login page
-    return <AdminLogin onLogin={setRole} />;
-  }
-
-  
-  // If logged in as admin or visitor, show simple message for now
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Welcome, {role}!</h1>
-      {role === "admin" ? (
-        <p>You can perform CRUD operations.</p>
-      ) : (
-        <p>You can only view products.</p>
-      )}
-      <button
-        onClick={() => {
-          localStorage.clear();
-          setRole(null);
-        }}
-      >
-        Logout
-      </button>
-    </div>
-  );
+  return <div>{isAdmin ? <AdminProducts /> : <VisitorProducts />}</div>;
 }
