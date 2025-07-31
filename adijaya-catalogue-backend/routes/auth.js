b190353c040a54f9a0c5d41818902ec71de763eb
Eldrian <eldrianayera710@gmail.com>
2025-07-31 18:03:01 +0800

    feat(backend) : create login path routes/auth.js

---
+++ adijaya-catalogue-backend/routes/auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const pool = require("../db");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM users WHERE username=$1 AND password=$2",
+      [username, password]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const user = result.rows[0];
+
+    // create Token
+    const token = jwt.sign(
+      { id: user.id, role: useReducer.role },
+      process.env.JWT_SECRET || "secretkey",
+      { expiresIn: "1h" }
+    );
+
+    res.json({ token, role: user.role });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
