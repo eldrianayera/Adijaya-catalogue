c60a988b4a8e2f7c0583b56fda3c769c4fb18139
Eldrian <eldrianayera710@gmail.com>
2025-08-03 22:24:04 +0800

    feat(FE): navbar FitWear

---
+++ frontend/src/components/shared-components/CardContainer.jsx
@@ -0,0 +1,34 @@
+import ProductsCards from "./ProductsCard";
+
+export default function CardContainer(props) {
+  return (
+    <div className="flex flex-wrap gap-3 justify-center">
+      {" "}
+      {props.products.map((product) => (
+        <div key={product.id} className="rounded-lg">
+          {" "}
+          {/* Product card content */}
+          <ProductsCards product={product} />
+          {/* Admin action buttons */}
+          {props.isValidAdmin && (
+            <div className="mt-2 flex gap-2 justify-center">
+              {" "}
+              <button
+                onClick={() => props.handleEdit(product)}
+                className="border-2 px-1"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => props.handleDelete(product.id)}
+                className="border-2 px-1"
+              >
+                Delete
+              </button>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ frontend/src/pages/PublicPage.jsx
@@ -0,0 +1,63 @@
+import CategoryBar from "../components/shared-components/CategoryBar";
+import CardContainer from "../components/shared-components/CardContainer";
+import { useAdmin } from "../hooks/useAdmin";
+import EditProductWindow from "../components/admin-components/EditProductWindow";
+import HeroContainer from "../components/shared-components/HeroContainer";
+
+export default function PublicPage() {
+  const {
+    products,
+    loading,
+    fetchProducts,
+    fetchProductsByCategory,
+    category,
+    isValidAdmin,
+    handleAdd,
+    handleDelete,
+    handleEdit,
+    handleSaveEdit,
+    isEditing,
+    setIsEditing,
+  } = useAdmin();
+
+  if (loading) return <p>Loading...</p>;
+
+  return (
+    <div>
+      {isEditing && (
+        <EditProductWindow
+          product={isEditing}
+          setIsEditing={setIsEditing}
+          handleSaveEdit={handleSaveEdit}
+          category={category}
+          handleAdd={handleAdd}
+        />
+      )}
+
+      <main className="px-9 flex flex-col gap-12 pt-30">
+        <HeroContainer />
+        <HeroContainer />
+        <CategoryBar
+          category={category}
+          fetchCategories={fetchProductsByCategory}
+          fetchProducts={fetchProducts}
+        />
+        {isValidAdmin && (
+          <div>
+            {" "}
+            <button onClick={handleAdd} className="border-2 px-1">
+              Add a New Product
+            </button>
+          </div>
+        )}
+        <CardContainer
+          products={products}
+          handleAdd={handleAdd}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          isValidAdmin={isValidAdmin}
+        />
+      </main>
+    </div>
+  );
+}
