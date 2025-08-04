3fc7a8c23b525aa1eec4183cdf405b25bb6bc77e
Eldrian <eldrianayera710@gmail.com>
2025-08-04 17:58:52 +0800

    feat(hero and about): add text and UI

--- frontend/src/components/index-page-components/HeroContainer1.jsx
+++ frontend/src/components/index-page-components/HeroContainer1.jsx
@@ -2,20 +2,22 @@ export default function HeroContainer1() {
   return (
     <section
       id="hero"
-      className="scroll-mt-30 h-120 flex flex-col justify-center items-center bg-cover "
+      className="scroll-mt-30 h-120 flex flex-col justify-center bg-cover px-20 gap-4"
       style={{
         backgroundImage: "url('homebg2.png')",
       }}
     >
-      <div className="absolute inset-0 bg-gray-500/10"></div>
-      <h1 className="text-5xl text-primary font-bold">
+      {/* <div className="absolute inset-0 bg-gray-500/10"></div> */}
+      <h1 className="text-6xl font-bold poppins">
         Gear Up. Push Harder. <br /> Perform Better.
       </h1>
-      <p>
+      <p className="text-gray-500">
         Elevate your game with high-performance activewear designed to move with
-        you. Stay comfortable, look sharp, and conquer every workout.
+        you. <br /> Stay comfortable, look sharp, and conquer every workout.
       </p>
-      <a href="/#product">Shop Now</a>
+      <a href="/#product" className="primary-button w-40 text-center">
+        Shop Now
+      </a>
     </section>
   );
 }
--- frontend/src/components/index-page-components/HeroContainer2.jsx
+++ frontend/src/components/index-page-components/HeroContainer2.jsx
@@ -1,11 +1,24 @@
 export default function HeroContainer2() {
   return (
-    <section id="hero" className="relative border-2 text-center scroll-mt-30">
-      <img
-        src="hero2.png"
-        alt="Sport apparel hero banner"
-        className="w-full h-auto object-cover"
-      />
+    <section
+      id="hero"
+      className="
+        relative border-2 text-center scroll-mt-30 h-120 overflow-hidden flex justify-end
+        before:content-[''] before:absolute before:inset-0
+        before:bg-[url('runner.png')] before:bg-[50%_100%] before:bg-cover
+        before:scale-x-[-1] before:z-0
+      "
+    >
+      <div className="relative z-10 border-8 text-right px-20 flex flex-col justify-center ">
+        <h1 className="text-6xl font-bold poppins text-primary">
+          Train Strong. <br /> Achieve More.
+        </h1>
+        <p className="text-background">
+          Unlock your full potential with apparel built for performance and
+          style.
+          <br /> Stay light, stay powerful, and own every moment.
+        </p>
+      </div>
     </section>
   );
 }
