24ab2802a9a2ce3ce5e2386fbea3e9f3d7851a55
Eldrian <eldrianayera710@gmail.com>
2025-08-05 16:43:14 +0800

    fix(FE): fix runner.png path

--- frontend/src/components/index-page-components/HeroContainer2.jsx
+++ frontend/src/components/index-page-components/HeroContainer2.jsx
@@ -5,7 +5,7 @@ export default function HeroContainer2() {
       className="
         relative border-2 text-right scroll-mt-30 h-120 overflow-hidden flex justify-end items-center
         before:content-[''] before:absolute before:inset-0
-        before:bg-[url('runner.png')] before:bg-[50%_100%] before:bg-cover
+        before:bg-[url('/runner.png')] before:bg-[50%_100%] before:bg-cover
         before:scale-x-[-1] before:z-0 before:pointer-events-none
       "
       aria-label="Hero section with background image and call to action"
